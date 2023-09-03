ab2228b4505b853b741c6386dec367df25674ca0
DanielHzp <124480168+DanielHzp@users.noreply.github.com>
2023-09-03 18:59:40 -0500

    Forms Navigation Added

---
+++ RazorPurchaseOrdersApp/Pages/Burger.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPurchaseOrdersApp.Pages
+{
+    public class BurgerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ RazorPurchaseOrdersApp/Pages/Forms/CustomBurger.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPurchaseOrdersApp.Pages.Forms
+{
+    public class CustomBurgerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
