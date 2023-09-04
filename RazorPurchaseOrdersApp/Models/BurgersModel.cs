4e98e72fa4a082d077f65a3137ff9aa87cefedf5
DanielHzp <124480168+DanielHzp@users.noreply.github.com>
2023-09-03 22:58:22 -0500

    Razor Pages View cshtml Styling

---
+++ RazorPurchaseOrdersApp/Models/BurgersModel.cs
@@ -0,0 +1,32 @@
+﻿namespace RazorPurchaseOrdersApp.Models
+{
+    public class BurgersModel
+    {
+        public string ImageTitle { get; set; }   
+
+        public string Name { get; set; }
+
+        public float UnitPrice { get; set; } = 5;
+
+        public bool HouseSauce { get; set; }
+
+        public bool Cheese { get; set; }
+
+        public bool HouseVeggies { get; set; }
+
+        public bool SingleMeat { get; set; }
+
+        public bool DoubleMeat { get;set; }
+
+        public bool ExtraBacon { get; set; }
+
+        public bool RazorVeggieToppings { get; set; }  
+
+
+        public float FinalPrice { get; set; }
+
+
+
+
+    }
+}
--- RazorPurchaseOrdersApp/Pages/Forms/CustomBurger.cshtml.cs
+++ RazorPurchaseOrdersApp/Pages/Forms/CustomBurger.cshtml.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPurchaseOrdersApp.Models;
 
 namespace RazorPurchaseOrdersApp.Pages.Forms
 {
+
+    //View model
     public class CustomBurgerModel : PageModel
     {
-        public void OnGet()
-        {
-        }
+        //Data binding between burger model and custom burger razor page view
+
+        [BindProperty]
+
+
+         //Main Class object reference (burger model)
+        public BurgersModel Burger { get; set; }
+        
+
+        
     }
 }
