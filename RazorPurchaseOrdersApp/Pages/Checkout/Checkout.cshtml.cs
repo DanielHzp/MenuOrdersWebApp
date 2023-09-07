70cc68b957144eff82592b15b60e64a1e06e7841
DanielHzp <124480168+DanielHzp@users.noreply.github.com>
2023-09-07 00:22:17 -0500

    CustomBurgers View Updated

--- RazorPurchaseOrdersApp/Pages/Checkout/Checkout.cshtml.cs
+++ RazorPurchaseOrdersApp/Pages/Checkout/Checkout.cshtml.cs
@@ -3,8 +3,21 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPurchaseOrdersApp.Pages.Checkout
 {
+
+    //Data binding property for the entire class
+    [BindProperties(SupportsGet =true)]
     public class CheckoutModel : PageModel
     {
+
+      
+
+        public string burgerName { get; set; }
+        public float BurgerPrice { get; set; }
+        public string ImageName { get; set; }
+
+
+
+        //Get data
         public void OnGet()
         {
         }
--- RazorPurchaseOrdersApp/Pages/Forms/CustomBurger.cshtml.cs
+++ RazorPurchaseOrdersApp/Pages/Forms/CustomBurger.cshtml.cs
@@ -15,6 +15,33 @@ namespace RazorPurchaseOrdersApp.Pages.Forms
 
          //Main Class object reference (burger model)
         public BurgersModel Burger { get; set; }
+
+        public float BurgerPrice { get; set; }
+
+        //IActionResult method
+        //Specify how a response is going to be given to a specific request
+        //Catch anything submitted with the POST method  (OnPost)
+        public IActionResult OnPost()
+        {
+            //Return response or error
+
+            BurgerPrice = Burger.UnitPrice;
+
+            //Set unit price increments based on boolean selection on this view
+            if (Burger.HouseSauce) BurgerPrice += 1;
+            if(Burger.Cheese) BurgerPrice += 1;
+            if (Burger.HouseVeggies) BurgerPrice += 1;
+            if (Burger.SingleMeat) BurgerPrice += 5;
+            if (Burger.DoubleMeat) BurgerPrice += 15;
+            if (Burger.ExtraBacon) BurgerPrice += 2;
+            if (Burger.RazorVeggieToppings) BurgerPrice += 3;
+
+            //Specify to which page to redirect and what attributes to pass
+            return RedirectToPage("/Checkout/Checkout", new {Burger.Name, BurgerPrice });
+
+
+        }
+
         
 
         
